5d39396629e6ddbfe40459ff458cb9bfc71cd579
loki156 <85053221+loki156@users.noreply.github.com>
2023-03-24 11:57:48 +0100

    Update matrix.js

--- custom-layout/js/matrix.js
+++ custom-layout/js/matrix.js
@@ -1,8 +1,8 @@
 /**
-* @version 2.0.3
+* @version 2.0.5
 * @author Serhiy Hembarevskyy (https://www.matrix-themes.com/)
 * @description Custom plugins for Matrix themes
-* @updated 04-03-2023
+* @updated 23-03-2023
 
 
 /*! jQuery v3.6.0 | (c) OpenJS Foundation and other contributors | jquery.org/license */
@@ -116,6 +116,48 @@ animateIn:!1},e.prototype.swap=function(){if(1===this.core.settings.items&&a.sup
  * @version 3.0.0
  */
 !function(a,b,c){var e=function(d,e){this.elem=d,this.$elem=a(d),this.options=e,this.metadata=this.$elem.data("plugin-options"),this.$win=a(b),this.sections={},this.didScroll=!1,this.$doc=a(c),this.docHeight=this.$doc.height()};e.prototype={defaults:{navItems:"a",currentClass:"current",changeHash:!1,easing:"swing",filter:"",scrollSpeed:750,scrollThreshold:.5,begin:!1,end:!1,scrollChange:!1},init:function(){return this.config=a.extend({},this.defaults,this.options,this.metadata),this.$nav=this.$elem.find(this.config.navItems),""!==this.config.filter&&(this.$nav=this.$nav.filter(this.config.filter)),this.$nav.on("click.onePageNav",a.proxy(this.handleClick,this)),this.getPositions(),this.bindInterval(),this.$win.on("resize.onePageNav",a.proxy(this.getPositions,this)),this},adjustNav:function(a,b){a.$elem.find("."+a.config.currentClass).removeClass(a.config.currentClass),b.addClass(a.config.currentClass)},bindInterval:function(){var b,a=this;a.$win.on("scroll.onePageNav",function(){a.didScroll=!0}),a.t=setInterval(function(){b=a.$doc.height(),a.didScroll&&(a.didScroll=!1,a.scrollChange()),b!==a.docHeight&&(a.docHeight=b,a.getPositions())},250)},getHash:function(a){return a.attr("href").split("#")[1]},getPositions:function(){var c,d,e,b=this;b.$nav.each(function(){c=b.getHash(a(this)),e=a("#"+c),e.length&&(d=e.offset().top,b.sections[c]=Math.round(d))})},getSection:function(a){var b=null,c=Math.round(this.$win.height()*this.config.scrollThreshold);for(var d in this.sections)this.sections[d]-c<a&&(b=d);return b},handleClick:function(c){var d=this,e=a(c.currentTarget),f=e.parent(),g="#"+d.getHash(e);f.hasClass(d.config.currentClass)||(d.config.begin&&d.config.begin(),d.adjustNav(d,f),d.unbindInterval(),d.scrollTo(g,function(){d.config.changeHash&&(b.location.hash=g),d.bindInterval(),d.config.end&&d.config.end()})),c.preventDefault()},scrollChange:function(){var c,a=this.$win.scrollTop(),b=this.getSection(a);null!==b&&(c=this.$elem.find('a[href$="#'+b+'"]').parent(),c.hasClass(this.config.currentClass)||(this.adjustNav(this,c),this.config.scrollChange&&this.config.scrollChange(c)))},scrollTo:function(b,c){var d=a(b).offset().top;a("html, body").animate({scrollTop:d},this.config.scrollSpeed,this.config.easing,c)},unbindInterval:function(){clearInterval(this.t),this.$win.unbind("scroll.onePageNav")}},e.defaults=e.prototype.defaults,a.fn.onePageNav=function(a){return this.each(function(){new e(this,a).init()})}}(jQuery,window,document);
+
+
+/*!
+ * Scaled text for Matrix Themes and Matrix Page Builder
+ * @version 1.0.4
+ * licensed under MIT
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the parent div elements with the classes 'j-text', 'is-scaled', 'is-fluid'
+  const parentDivs = document.querySelectorAll('.j-text, .is-fluid, .is-scaled');
+
+  // Loop through each parent div and set the font size of all the text span elements inside it
+  function setFontSizes() {
+parentDivs.forEach(function(parentDiv) {
+  const textSpans = parentDiv.querySelectorAll('span');
+  if (textSpans.length > 0) {
+    const parentWidth = parentDiv.offsetWidth;
+    textSpans.forEach(function(textSpan) {
+      const currentFontSize = parseFloat(window.getComputedStyle(textSpan).fontSize);
+      const initialFontSize = parseFloat(textSpan.style.fontSize);
+      let fontSize = Math.floor(parentWidth / 10); // Adjust this factor to control the scaling
+      fontSize = Math.max(fontSize, 50); // set minimum font size to 50px
+      if (currentFontSize > 50) {
+        if (fontSize > initialFontSize) {
+          fontSize = initialFontSize;
+        }
+        textSpan.style.fontSize = `${fontSize}px`;
+        textSpan.style.wordBreak = "break-word"; // Add this line to enable breaking long words
+      }
+    });
+  }
+});
+}
+
+  // Call the setFontSizes function initially to set the font size of the text for all instances
+  setTimeout(setFontSizes, 0);
+
+  // Call the setFontSizes function whenever the window is resized to adjust the font size for all instances
+  window.addEventListener('resize', setFontSizes);
+});
+
 
 // Matrix Init
 jQuery.noConflict();
